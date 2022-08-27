6d6b042329e76bdd73c1985261ee6a615a046dd7
VinayagamD <vinayagam.d.ganesh@gmail.com>
2022-08-28 00:45:53 +0530

    Added ISP interface ThreeDimensionalShape

--- src/main/java/com/vinaylogics/solid/demo/models/Cube.java
+++ src/main/java/com/vinaylogics/solid/demo/models/Cube.java
@@ -1,8 +1,9 @@
 package com.vinaylogics.solid.demo.models;
 
+import com.vinaylogics.solid.demo.solid.isp.ThreeDimensionalShape;
 import com.vinaylogics.solid.demo.solid.ocp.Shape;
 
-public class Cube implements Shape {
+public class Cube implements Shape, ThreeDimensionalShape {
     private int area;
 
     public Cube(int area) {
@@ -12,5 +13,10 @@ public class Cube implements Shape {
     @Override
     public double area() {
         return 6 * Math.pow(area, 2);
+    }
+
+    @Override
+    public double volume() {
+        return Math.pow(area, 3);
     }
 }
---
+++ src/main/java/com/vinaylogics/solid/demo/solid/isp/ThreeDimensionalShape.java
@@ -0,0 +1,6 @@
+package com.vinaylogics.solid.demo.solid.isp;
+
+public interface ThreeDimensionalShape {
+
+    double volume();
+}
